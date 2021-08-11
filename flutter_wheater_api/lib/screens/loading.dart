d64d265626ebe20ce0063882271e0d01979c01c0
Yxxnsu <dustn0121@naver.com>
2021-08-11 11:02:18 +0900

    [ADD] basic json decoding setting

--- flutter_wheater_api/lib/screens/loading.dart
+++ flutter_wheater_api/lib/screens/loading.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   Loading({Key? key}) : super(key: key);
@@ -13,6 +15,7 @@ class _LoadingState extends State<Loading> {
   @override
   void initState() {
     getLocation();    
+    fetchMethod();
     super.initState();
   }
 
@@ -22,6 +25,17 @@ class _LoadingState extends State<Loading> {
       print(position);      
     } catch(e){
       print('Internet에 문제가 생겼습니다');
+    }
+  }
+
+  void fetchMethod() async {
+    http.Response response = await http.get(Uri.parse('https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1'));
+    if(response.statusCode == 200){
+      String jsonData = response.body;
+      var myJson = jsonDecode(jsonData)['wind']['speed'];
+      var myId = jsonDecode(jsonData)['id'];
+      print(myJson);
+      print(myId);
     }
   }
 
